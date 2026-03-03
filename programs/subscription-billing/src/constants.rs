ee1ec040458ee48df54753632c1e6bbdb8d1ea30
PuroRoxo <puroroxoltd@gmail.com>
2026-03-03 11:51:21 -0300

    Add programs/subscription-billing/src/constants.rs

---
+++ programs/subscription-billing/src/constants.rs
@@ -0,0 +1,4 @@
+pub const MAX_PLATFORM_FEE_BPS: u16 = 1000; // 10%
+pub const MAX_PERIODS_PER_PAYMENT: u8 = 12; // Max 1 year upfront
+pub const MIN_PERIOD_DURATION: i64 = 86400; // 1 day minimum
+pub const MAX_PERIOD_DURATION: i64 = 31536000; // 1 year maximum
